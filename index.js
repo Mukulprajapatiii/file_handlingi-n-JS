d2f4256a70a2e2670cae8cb13a44e39295df7e36
mukulprajapatiii <mukulprajapati1319@gmail.com>
2024-06-29 20:55:44 +0530

    second-commit

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-ffwdd
+const { error } = require("console");
+const fs = require("fs");
+
+//This was synchronus call...
+fs.writeFileSync("./text.txt","Hey");
+
+//This is async call ...
+fs.writeFile("./test.txt","Hey.. This is async call", (error)=>{
+
+});
+
+
+//Sync call...
+
+// const result = fs.readFileSync("./read_file.txt","utf-8");
+// console.log(result);
+
+
+//Async call ...
+
+fs.readFile("./read_file.txt","utf-8", (error, result)=>{
+    if(error){
+        console.log("Error came");
+    }
+    else{
+        console.log(result);
+    }
+});
